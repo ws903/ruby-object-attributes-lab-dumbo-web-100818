ad5ae8d4ba4a1dd4a1075197b6f18309661fe8a5
David Sim <wsim903@gmail.com>
2018-10-09 20:14:35 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def name
+    puts @name
+  end
+  
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+  
+  def breed
+    puts @breed
+  end
+end
